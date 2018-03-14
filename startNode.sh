c415a59838742cc0152270e8ab55a9c440875a94
Mats Bovin <mats.bovin@gmail.com>
2018-03-14 15:14:48 +0100

    Bumped driver versions

--- startNode.sh
+++ startNode.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SELENIUMSERVER_BASEURL="http://selenium-release.storage.googleapis.com/3.5/";
-SELENIUMSERVER_NAME="selenium-server-standalone-3.5.3.jar";
+SELENIUMSERVER_BASEURL="http://selenium-release.storage.googleapis.com/3.9/";
+SELENIUMSERVER_NAME="selenium-server-standalone-3.9.1.jar";
 CHROMEDRIVER_BASEURL="http://chromedriver.storage.googleapis.com/";
-CHROMEDRIVER_VERSION="2.32/";
+CHROMEDRIVER_VERSION="2.36/";
 CHROMEDRIVER_NAME="chromedriver_mac64.zip";
 if [ ! -f $SELENIUMSERVER_NAME ]; then
 	curl -O $SELENIUMSERVER_BASEURL$SELENIUMSERVER_NAME;
